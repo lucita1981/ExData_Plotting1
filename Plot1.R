1d847e13aabd6a3a9bcf3aad737d94e8f3945732
lucita1981 <36177396+lucita1981@users.noreply.github.com>
2018-03-09 15:20:59 -0500

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,9 @@
+#Reading the Data
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";")
+subdata <- subset(data, data$Date=="1/2/2007" | data$Date =="2/2/2007")
+
+#Plot function & Using PNG Device
+GlobalActivePower <- as.numeric(subdata$Global_active_power)
+png("Plot1.png", width=480, height=480)
+hist(GlobalActivePower, col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)", ylab = "Frecuency")
+dev.off()
